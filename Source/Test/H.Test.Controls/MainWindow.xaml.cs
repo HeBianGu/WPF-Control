b9ce87ee16299f0d6c84d1fd11bb2ee51a3e237c
HeBianGu <908293466@qq.com>
2023-10-26 18:34:38 +0800

    初始化

---
+++ Source/Test/H.Test.Controls/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H.Test.Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+    public class Student : ICloneable
+    {
+        [Display(Name = "姓名", GroupName = "基础信息")]
+        [Required()]
+        public string Name { get; set; }
+
+        [Display(Name = "班级", GroupName = "基础信息")]
+        [Required]
+        public string Class { get; set; }
+
+        [Display(Name = "地址", GroupName = "基础信息")]
+        [Required]
+        public string Address { get; set; }
+
+        [Display(Name = "邮箱", GroupName = "基础信息")]
+        [Required]
+        public string Emall { get; set; }
+
+        [Display(Name = "可用", GroupName = "其他信息")]
+        [Required]
+        public bool IsEnbled { get; set; }
+
+        [Display(Name = "时间", GroupName = "其他信息")]
+        [Required]
+        public DateTime Time { get; set; }
+
+        [Display(Name = "年龄", GroupName = "基础信息")]
+        [Required]
+        public int Age { get; set; }
+
+        [Display(Name = "分数", GroupName = "成绩信息")]
+        public double Score { get; set; }
+
+        [Display(Name = "电话号码", GroupName = "基础信息")]
+        [Required]
+        [RegularExpression(@"^1[3|4|5|7|8][0-9]{9}$", ErrorMessage = "手机号码不合法！")]
+        public string Tel { get; set; }
+
+        private static Random random = new Random();
+        private static string[] names = new string[] { "Lucy", "Jim", "Tom", "Lulu", "Angle", "Linda", "Summi" };
+        private static string[] mails = new string[] { "@163.com", "@126.com", "@qq.com", "@gmail.com" };
+        private static string[] address = new string[] { "浙江省 台州市 临海市", "广西壮族自治区 钦州市 钦北区", "重庆 重庆市 南岸区", "上海 上海市 闵行区", "香港特别行政区 香港岛 南区", "新疆维吾尔自治区 喀什地区 伽师县", "Summi" };
+        public Student()
+        {
+            this.Name = names[random.Next(names.Count())];
+            this.Class = random.Next(1, 6).ToString() + "班";
+            this.Address = address[random.Next(address.Count())];
+            this.Emall = random.Next(0, 99999999).ToString().PadLeft(8, '0') + mails[random.Next(mails.Count())];
+            this.IsEnbled = random.Next(1, 3) == 2;
+            this.Time = DateTime.Now.AddDays(random.Next(-50, 50));
+            this.Age = random.Next(10, 50);
+            this.Score = Math.Round(random.NextDouble() * 100, 2);
+            this.Tel = "13" + random.Next(9) + random.Next(0, 99999999).ToString().PadLeft(8, '0');
+        }
+
+        public static Student Random()
+        {
+            Student student = new Student();
+            student.Name = names[random.Next(names.Count())];
+            student.Class = random.Next(1, 6).ToString() + "班";
+            student.Address = address[random.Next(address.Count())];
+            student.Emall = random.Next(0, 99999999).ToString().PadLeft(8, '0') + mails[random.Next(mails.Count())];
+            student.IsEnbled = random.Next(1, 3) == 2;
+            student.Time = DateTime.Now.AddDays(random.Next(-50, 50));
+            student.Age = random.Next(10, 50);
+            student.Score = Math.Round(random.NextDouble() * 100, 2);
+            student.Tel = "13" + random.Next(9) + random.Next(0, 99999999).ToString().PadLeft(8, '0');
+            return student;
+        }
+
+        public object Clone()
+        {
+            return Student.Random();
+        }
+
+        public static List<Student> Randoms(int c = 100)
+        {
+            return Enumerable.Range(0, c).Select(x => Student.Random()).ToList();
+        }
+    }
+
+    public class GetStudentsExtension : MarkupExtension
+    {
+        public int Count { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return Enumerable.Range(0, this.Count).Select(x => new Student());
+        }
+    }
+
+    public class StudentValidationRule : ValidationRule
+    {
+        public override System.Windows.Controls.ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is Student student)
+                return student.Age > 30 ? System.Windows.Controls.ValidationResult.ValidResult : new System.Windows.Controls.ValidationResult(false, "Age不应该大于50");
+            return System.Windows.Controls.ValidationResult.ValidResult;
+        }
+    }
+}
---
+++ Source/Theme/H.Themes.Default/ResourceKey.cs
@@ -0,0 +1,143 @@
+﻿// Copyright © 2022 By HeBianGu(QQ:908293466) https://github.com/HeBianGu/WPF-ControlBase
+
+using System;
+using System.Windows;
+
+namespace H.Themes.Default
+{
+    public static class ColorKeys
+    {
+        #region - Background -
+        public static ComponentResourceKey Background => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextBackground");
+        public static ComponentResourceKey BackgroundDisabled => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextBackground.Disabled");
+        public static ComponentResourceKey AlternatingRowBackground => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.RowIndex.BackGround");
+        #endregion
+
+        public static ComponentResourceKey Foreground => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground");
+        public static ComponentResourceKey ForegroundTitle => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.Title");
+        public static ComponentResourceKey ForegroundDisabled => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.Disabled");
+
+        #region - Foreground -
+
+        public static ComponentResourceKey MouseOver => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextMouseOver");
+        public static ComponentResourceKey Selected => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextSelected");
+        public static ComponentResourceKey ForegroundAssist => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.Assist");
+        public static ComponentResourceKey ForegroundLink => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.Link");
+        public static ComponentResourceKey ForegroundWhite => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White");
+
+        public static ComponentResourceKey ForegroundWhiteOpacity9 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.9");
+        public static ComponentResourceKey ForegroundWhiteOpacity8 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.8");
+        public static ComponentResourceKey ForegroundWhiteOpacity7 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.7");
+        public static ComponentResourceKey ForegroundWhiteOpacity6 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.6");
+        public static ComponentResourceKey ForegroundWhiteOpacity5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.5");
+        public static ComponentResourceKey ForegroundWhiteOpacity4 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.4");
+        public static ComponentResourceKey ForegroundWhiteOpacity3 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.3");
+        public static ComponentResourceKey ForegroundWhiteOpacity2 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.2");
+        public static ComponentResourceKey ForegroundWhiteOpacity1 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextForeground.White.Opacity.1");
+
+        #endregion
+
+        #region - BorderBrush -
+
+        public static ComponentResourceKey BorderBrush => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextBorderBrush");
+        public static ComponentResourceKey BorderBrushTitle => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextBorderBrush.Title");
+        public static ComponentResourceKey BorderBrushAssist => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextBorderBrush.Assist");
+        public static ComponentResourceKey BorderBrushDisabled => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.TextBorderBrush.Disabled");
+        #endregion
+
+
+        #region - Accent -
+        public static ComponentResourceKey Accent => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Accent");
+        #endregion 
+
+        #region - Dark - 
+        public static ComponentResourceKey Dark10 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.10");
+        public static ComponentResourceKey Dark9_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.9.5");
+        public static ComponentResourceKey Dark9 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.9");
+        public static ComponentResourceKey Dark8_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.8.5");
+        public static ComponentResourceKey Dark8 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.8");
+        public static ComponentResourceKey Dark7_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.7.5");
+        public static ComponentResourceKey Dark7 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.7");
+        public static ComponentResourceKey Dark6_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.6.5");
+        public static ComponentResourceKey Dark6 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.6");
+        public static ComponentResourceKey Dark5_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.5.5");
+        public static ComponentResourceKey Dark5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.5");
+        public static ComponentResourceKey Dark4_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.4.5");
+        public static ComponentResourceKey Dark4 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.4");
+        public static ComponentResourceKey Dark3_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.3.5");
+        public static ComponentResourceKey Dark3 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.3");
+        public static ComponentResourceKey Dark2_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.2.5");
+        public static ComponentResourceKey Dark2 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.2");
+        public static ComponentResourceKey Dark1_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.1.5");
+        public static ComponentResourceKey Dark1 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.1");
+        public static ComponentResourceKey Dark0_9 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.9");
+        public static ComponentResourceKey Dark0_8 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.8");
+        public static ComponentResourceKey Dark0_7 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.7");
+        public static ComponentResourceKey Dark0_6 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.6");
+        public static ComponentResourceKey Dark0_5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.5");
+        public static ComponentResourceKey Dark0_4 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.4");
+        public static ComponentResourceKey Dark0_3 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.3");
+        public static ComponentResourceKey Dark0_2 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.2");
+        public static ComponentResourceKey Dark0_1 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0.1");
+        public static ComponentResourceKey Dark0 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dark.0");
+        #endregion 
+
+        #region - Color -
+        public static ComponentResourceKey LightGray => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.LightGray.Notice");
+        public static ComponentResourceKey LightGrayOpacity5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.LightGray.NoticeOpacity.5");
+        public static ComponentResourceKey Gray => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Gray.Notice");
+        public static ComponentResourceKey GrayOpacity5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Gray.Notice.Opacity.5,");
+        public static ComponentResourceKey Black => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Black.Notice");
+        public static ComponentResourceKey Orange => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Orange.Notice");
+        public static ComponentResourceKey Red => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Red.Notice");
+        public static ComponentResourceKey Green => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Green.Notice");
+        public static ComponentResourceKey Yellow => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Yellow.Notice");
+        public static ComponentResourceKey Blue => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Blue.Notice");
+        public static ComponentResourceKey Purple => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Purple.Notice");
+        public static ComponentResourceKey Brown => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Brown.Notice");
+        public static ComponentResourceKey LightBlue => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.LightBlue.Notice");
+        public static ComponentResourceKey Pink => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Pink.Notice");
+
+        public static ComponentResourceKey White => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Notice");
+        public static ComponentResourceKey WhiteOpactiy9 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.9");
+        public static ComponentResourceKey WhiteOpactiy8 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.8");
+        public static ComponentResourceKey WhiteOpactiy7 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.7");
+        public static ComponentResourceKey WhiteOpactiy6 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.6");
+        public static ComponentResourceKey WhiteOpactiy5 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.5");
+        public static ComponentResourceKey WhiteOpactiy4 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.4");
+        public static ComponentResourceKey WhiteOpactiy3 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.3");
+        public static ComponentResourceKey WhiteOpactiy2 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.2");
+        public static ComponentResourceKey WhiteOpactiy1 => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.White.Opactiy.1");
+
+        #endregion
+
+        #region - System -
+        public static ComponentResourceKey DialogCover => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Dialog.Cover");
+
+        #endregion
+
+        public static ComponentResourceKey Tranparent => new ComponentResourceKey(typeof(ColorKeys), "S.Brush.Tranparent");
+    }
+
+    public static class LayoutKeys
+    {
+        public static ComponentResourceKey WindowCaptionHeight => new ComponentResourceKey(typeof(LayoutKeys), "S.Layout.WindowCaptionHeight");
+        public static ComponentResourceKey RowHeight => new ComponentResourceKey(typeof(LayoutKeys), "S.Layout.RowHeight");
+        public static ComponentResourceKey ItemHeight => new ComponentResourceKey(typeof(LayoutKeys), "S.Layout.ItemHeight");
+    }
+
+    public static class FontSizeKeys
+    {
+        public static ComponentResourceKey Header => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header");
+        public static ComponentResourceKey Header1 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.1");
+        public static ComponentResourceKey Header2 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.2");
+        public static ComponentResourceKey Header3 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.3");
+        public static ComponentResourceKey Header4 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.4");
+        public static ComponentResourceKey Header5 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.5");
+        public static ComponentResourceKey Header6 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.6");
+        public static ComponentResourceKey Header7 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.7");
+        public static ComponentResourceKey Header8 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.8");
+        public static ComponentResourceKey Header9 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.9");
+        public static ComponentResourceKey Default => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Default");
+    }
+}
