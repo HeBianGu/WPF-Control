764b20adec47936fa0821be61b44bb992e349572
HeBianGu <908293466@qq.com>
2023-10-27 11:12:33 +0800

    修改样式

---
+++ Source/Theme/H.Themes.Default/Commands.Window.cs
@@ -0,0 +1,71 @@
+﻿// Copyright © 2022 By HeBianGu(QQ:908293466) https://github.com/HeBianGu/WPF-ControlBase
+
+using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Markup;
+
+namespace H.Themes.Default
+{
+    public abstract class WindowCommandBase : MarkupExtension, ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+        public bool CanExecute(object? parameter)
+        {
+            return parameter is Window;
+        }
+
+        public abstract void Execute(object? parameter);
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+    public class CloseWindowCommand : WindowCommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            if (parameter is Window window)
+                SystemCommands.CloseWindow(window);
+        }
+    }
+
+    public class RestoreWindowCommand : WindowCommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            if (parameter is Window window)
+                SystemCommands.RestoreWindow(window);
+        }
+    }
+
+    public class MaximizeWindowCommand : WindowCommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            if (parameter is Window window)
+                SystemCommands.MaximizeWindow(window);
+        }
+    }
+
+    public class MinimizeWindowCommand : WindowCommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            if (parameter is Window window)
+                SystemCommands.MinimizeWindow(window);
+        }
+    }
+}
---
+++ Source/Theme/H.Themes.Default/Keys/FontSizeKeys.xaml.cs
@@ -0,0 +1,22 @@
+﻿// Copyright © 2022 By HeBianGu(QQ:908293466) https://github.com/HeBianGu/WPF-ControlBase
+
+using System;
+using System.Windows;
+
+namespace H.Themes.Default
+{
+    public static class FontSizeKeys
+    {
+        public static ComponentResourceKey Header => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header");
+        public static ComponentResourceKey Header1 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.1");
+        public static ComponentResourceKey Header2 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.2");
+        public static ComponentResourceKey Header3 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.3");
+        public static ComponentResourceKey Header4 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.4");
+        public static ComponentResourceKey Header5 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.5");
+        public static ComponentResourceKey Header6 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.6");
+        public static ComponentResourceKey Header7 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.7");
+        public static ComponentResourceKey Header8 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.8");
+        public static ComponentResourceKey Header9 => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Header.9");
+        public static ComponentResourceKey Default => new ComponentResourceKey(typeof(FontSizeKeys), "S.FontSize.Default");
+    }
+}
