dfb2f77be5341d3e11760768a79fdc8ac09a0890
HeBianGu <908293466@qq.com>
2023-11-09 00:44:42 +0800

    Dock+

--- Source/Styles/H.Styles.Default/Controls/ToggleButton.xaml.cs
+++ Source/Styles/H.Styles.Default/Controls/ToggleButton.xaml.cs
@@ -10,5 +10,7 @@ namespace H.Styles.Default
     public class ToggleButtonKeys
     {
         public static ComponentResourceKey Default => new ComponentResourceKey(typeof(ToggleButtonKeys), "S.ToggleButton.Default");
+        public static ComponentResourceKey Geometry => new ComponentResourceKey(typeof(ToggleButtonKeys), "S.ToggleButton.Geometry");
+
     }
 }
