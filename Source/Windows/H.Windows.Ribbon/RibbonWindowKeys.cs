14dafe71d63d0cec703bc685af16a7aae4992e9b
HeBianGu <navylee0210@163.com>
2023-11-09 20:29:28 +0800

    Ribbon+

---
+++ Source/Windows/H.Windows.Ribbon/RibbonWindowKeys.cs
@@ -0,0 +1,16 @@
+﻿using H.Styles.Default;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace H.Windows.Ribbon
+{
+    public class RibbonWindowKeys
+    {
+        public static ComponentResourceKey Default => new ComponentResourceKey(typeof(RibbonWindowKeys), "S.RibbonWindow.Default");
+
+    }
+}
