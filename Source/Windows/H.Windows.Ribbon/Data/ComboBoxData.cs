52f66ebc1573946099f65cc29e8fa26882b0bc8f
HeBianGu <navylee0210@163.com>
2023-11-09 16:37:33 +0800

    Ribbon+

---
+++ Source/Windows/H.Windows.Ribbon/Data/ComboBoxData.cs
@@ -0,0 +1,8 @@
+﻿// Copyright © 2022 By HeBianGu(QQ:908293466) https://github.com/HeBianGu/WPF-ControlBase
+
+namespace H.Windows.Ribbon
+{
+    public class ComboBoxData : MenuButtonData
+    {
+    }
+}
