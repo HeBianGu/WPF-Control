b264adcadedaec065276c7d2310fc7e4ecc5980f
HeBianGu <908293466@qq.com>
2024-01-23 21:34:51 +0800

    Base+

---
+++ Source/Presenters/H.Presenters.Common/SelectItemPresenter.xaml.cs
@@ -0,0 +1,52 @@
+﻿using H.Providers.Mvvm;
+using System.Collections;
+
+namespace H.Presenters.Common
+{
+    public class SelectItemPresenter : DisplayViewModelBase
+    {
+        public SelectItemPresenter(IEnumerable source, object selectedItem)
+        {
+            this.Source = source;
+            this.SelectedItem = selectedItem;
+        }
+
+        private IEnumerable _source;
+        /// <summary> 说明  </summary>
+        public IEnumerable Source
+        {
+            get { return _source; }
+            set
+            {
+                _source = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _displayMemberPath;
+        /// <summary> 说明  </summary>
+        public string DisplayMemberPath
+        {
+            get { return _displayMemberPath; }
+            set
+            {
+                _displayMemberPath = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+        private object _selectedItem;
+        /// <summary> 说明  </summary>
+        public object SelectedItem
+        {
+            get { return _selectedItem; }
+            set
+            {
+                _selectedItem = value;
+                RaisePropertyChanged();
+            }
+        }
+
+    }
+}
