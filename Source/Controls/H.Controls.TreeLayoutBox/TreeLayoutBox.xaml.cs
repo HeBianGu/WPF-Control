65f45291ed3a7aaf49f9641b179521ec185b338f
HeBianGu <908293466@qq.com>
2024-06-29 18:36:00 +0800

    TreeLayoutBox+

---
+++ Source/Controls/H.Controls.TreeLayoutBox/TreeLayoutBox.xaml.cs
@@ -0,0 +1,25 @@
+﻿// Copyright © 2024 By HeBianGu(QQ:908293466) https://github.com/HeBianGu/WPF-Control
+
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace H.Controls.TreeLayoutBox
+{
+    public partial class TreeLayoutBox : TreeView
+    {
+        static TreeLayoutBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TreeLayoutBox), new FrameworkPropertyMetadata(typeof(TreeLayoutBox)));
+
+           
+        }
+    }
+
+    public partial class TreeLayoutBox
+    {
+        public static ComponentResourceKey VerticalKey => new ComponentResourceKey(typeof(TreeLayoutBox), "S.TreeLayoutBox.Vertical");
+
+        public static  ComponentResourceKey HorizontalKey => new ComponentResourceKey(typeof(TreeLayoutBox), "S.TreeLayoutBox.Horizontal");
+    }
+}
