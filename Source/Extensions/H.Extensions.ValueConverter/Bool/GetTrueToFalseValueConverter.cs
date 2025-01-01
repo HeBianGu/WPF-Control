d2a9be2ace993d6957547c439d61f64188b7c26e
HeBianGu <navylee0210@163.com>
2025-01-01 18:50:14 +0800

    #9

--- Source/Extensions/H.Extensions.ValueConverter/Bool/GetTrueToFalseValueConverter.cs
+++ Source/Extensions/H.Extensions.ValueConverter/Bool/GetTrueToFalseValueConverter.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿// Copyright © 2024 By HeBianGu(QQ:908293466) https://github.com/HeBianGu/WPF-Control
+
+using System;
 using System.Globalization;
 
 namespace H.Extensions.ValueConverter
 {
-    public class WriteObjectConverter : MarkupValueConverterBase
+    public class GetTrueToFalseValueConverter : MarkupValueConverterBase
     {
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            System.Diagnostics.Debug.WriteLine(value);
+            if (value is bool b)
+                return !b;
             return value;
         }
     }
