02e4ae5bedbef8b2b7a66f54e98861961b2498f2
HeBianGu <908293466@qq.com>
2025-03-07 12:20:10 +0800

    tt+

--- Source/Tests/H.Test.Form/AssemblyInfo.cs
+++ Source/Tests/H.Test.Form/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-global using System.Windows;
+using System.Windows;
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
                                      //(used if a resource is not found in the page,
