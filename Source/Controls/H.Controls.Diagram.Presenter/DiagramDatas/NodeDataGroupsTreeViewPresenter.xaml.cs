1f2e2426a8294d7d6b1b1e34a6284d316073060b
HeBianGu <908293466@qq.com>
2025-03-13 22:20:14 +0800

    ++

---
+++ Source/Controls/H.Controls.Diagram.Presenter/DiagramDatas/NodeDataGroupsTreeViewPresenter.xaml.cs
@@ -0,0 +1,32 @@
+﻿using H.Extensions.ValueConverter;
+using System.Globalization;
+
+namespace H.Controls.Diagram.Presenter.DiagramDatas;
+public class NodeDataGroupsTreeViewPresenter : DisplayBindableBase
+{
+    public NodeDataGroupsTreeViewPresenter(IEnumerable<INodeDataGroup> nodeDataGroups)
+    {
+        this._nodeDataGroups = nodeDataGroups;
+    }
+
+    private IEnumerable<INodeDataGroup> _nodeDataGroups;
+    public IEnumerable<INodeDataGroup> NodeDataGroups
+    {
+        get { return _nodeDataGroups; }
+        set
+        {
+            _nodeDataGroups = value;
+            RaisePropertyChanged();
+        }
+    }
+}
+
+public class GetNodeDataGroupsTreeViewPresenter : MarkupValueConverterBase
+{
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is IEnumerable<INodeDataGroup> nodegroups)
+            return new NodeDataGroupsTreeViewPresenter(nodegroups);
+        return null;
+    }
+}
