03bb86f5a916b81906f73b92a639f4cabc997d6a
HeBianGu <navylee0210@163.com>
2025-03-23 22:17:12 +0800

    Create addtag.bat

---
+++ Document/bat/addtag.bat
@@ -0,0 +1,11 @@
+:: 标签操作，使用 git tag -d <tag_name> 删除本地标签，使用 git push origin --delete <tag_name> 删除远程标签
+@echo off
+cd /d "%~dp0"
+:: 查看标签
+git tag
+:: 创建标签
+git tag v1.0.4
+:: 推送所有标签
+git push origin --tags
+pause
+
