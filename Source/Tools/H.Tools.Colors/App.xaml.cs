70bc39f87abfcdb354bd3d8aa003a49a3b2a8ecf
HeBianGu <navylee0210@163.com>
2025-03-31 16:58:33 +0800

    ++

--- Source/Tools/H.Tools.Colors/App.xaml.cs
+++ Source/Tools/H.Tools.Colors/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace H.App.Colors;
+namespace H.Tools.Colors;
 /// <summary>
 /// Interaction logic for App.xaml
 /// </summary>
--- Source/Tools/H.Tools.Colors/MainWindow.xaml.cs
+++ Source/Tools/H.Tools.Colors/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace H.App.Colors;
+namespace H.Tools.Colors;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
