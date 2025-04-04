085c62f69abc017af1dc1129adfb35733b909244
HeBianGu <908293466@qq.com>
2025-04-04 22:26:39 +0800

    Rename+

--- Source/Controls/H.Controls.Diagram.Presenters.OpenCV/OpenCV.xaml.cs
+++ Source/Controls/H.Controls.Diagram.Presenters.OpenCV/OpenCV.xaml.cs
@@ -2,7 +2,7 @@
 using H.Controls.Diagram.Presenters.OpenCV.NodeDataGroups;
 
 namespace H.Controls.Diagram.Presenters.OpenCV;
-[Display(Name = "OpenCV图像处理", GroupName = "机器视觉", Order = 0)]
+[Display(Name = "OpenCV图片处理", GroupName = "机器视觉", Order = 0)]
 public class OpenCVImageDiagramData : NodeDataGroupsDiagramDataBase
 {
     protected override IEnumerable<INodeDataGroup> CreateNodeGroups()
