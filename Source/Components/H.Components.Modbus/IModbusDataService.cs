59eee1ec4919ef67bf2eaaacf9d869eaffdb8db7
HeBianGu <navylee0210@163.com>
2025-05-12 00:07:43 +0800

    Modbus+

---
+++ Source/Components/H.Components.Modbus/IModbusDataService.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+namespace H.Components.Modbus;
+
+public interface IModbusDataService
+{
+    ModbusDatas Collection { get; set; }
+    List<ITcpClientMaster> Masters { get; set; }
+    ModbusDataState State { get; set; }
+    void Add(IModbusDataItem item);
+    bool CanStart();
+    bool CanStop();
+    void Clear();
+    List<ITcpClientMaster> CreateTcpClientMasters();
+    void Delete(IModbusDataItem item);
+    List<ITcpClientMaster> GetMasters();
+    void InvalidateMasters();
+    Task Start();
+    Task Stop();
+}
---
+++ Source/Components/H.Components.Modbus/Presenters/ModbusDataViewPresenter.xaml.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+global using H.Components.Modbus.Base;
+global using H.Iocable;
+global using H.Mvvm.Commands;
+global using H.Mvvm.ViewModels.Base;
+global using H.Services.Message;
+global using H.Services.Message.Dialog;
+global using System.ComponentModel.DataAnnotations;
+
+namespace H.Components.Modbus.Presenters;
+
+[Display(Name = "Modbus寄存器管理")]
+internal class ModbusDataViewPresenter : DisplayBindableBase
+{
+    public ModbusDataViewPresenter()
+    {
+        this.DataService = Ioc.GetService<ISerializableModbusDataService>();
+    }
+    private IModbusDataService _dataService;
+    public IModbusDataService DataService
+    {
+        get { return _dataService; }
+        set
+        {
+            _dataService = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    private IModbusDataItem _selectedItem;
+    public IModbusDataItem SelectedItem
+    {
+        get { return _selectedItem; }
+        set
+        {
+            _selectedItem = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public RelayCommand AddCommand => new RelayCommand(async x =>
+    {
+        UshortModbusDataItem item = new UshortModbusDataItem();
+        var r = await IocMessage.Form.ShowEdit(item, x => x.Title = "添加Modbus地址");
+        if (r != true)
+            return;
+        this.DataService.Add(item);
+    }, x => this.DataService.CanStart());
+
+    public RelayCommand DeleteCommand => new RelayCommand(async x =>
+    {
+        if (x is IModbusDataItem item)
+        {
+            await IocMessage.Dialog.ShowDeleteDialog(l =>
+            {
+                this.DataService.Delete(item);
+            });
+        }
+    }, x => this.DataService.CanStart());
+
+    public RelayCommand EditCommand => new RelayCommand(async x =>
+    {
+        if (x is IModbusDataItem item)
+            await IocMessage.Form.ShowEdit(item, x => x.Title = "编辑");
+    }, x => this.DataService.CanStart());
+
+    public RelayCommand ClearCommand => new RelayCommand(async x =>
+    {
+        await IocMessage.Dialog.ShowDeleteAllDialog(l =>
+        {
+            this.DataService.Clear();
+        });
+    }, x => this.DataService.Collection.Count > 0);
+
+    public RelayCommand StartCommand => new RelayCommand(async x =>
+    {
+        await this.DataService.Start();
+    }, x => this.DataService.CanStart());
+
+    public RelayCommand StopCommand => new RelayCommand(async x =>
+    {
+        await this.DataService.Stop();
+    }, x => this.DataService.CanStop());
+
+}
