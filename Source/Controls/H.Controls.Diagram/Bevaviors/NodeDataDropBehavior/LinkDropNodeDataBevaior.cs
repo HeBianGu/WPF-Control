e1270d1ed6889a7c9982cc36f2e9d061456349a4
HeBianGu <908293466@qq.com>
2025-06-07 21:04:11 +0800

    LinkDrop+

---
+++ Source/Controls/H.Controls.Diagram/Bevaviors/NodeDataDropBehavior/LinkDropNodeDataBevaior.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+namespace H.Controls.Diagram.Bevaviors.NodeDataDropBehavior;
+
+public class LinkDropNodeDataBevaior : DropNodeDataBehaviorBase<Link>
+{
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        this.AssociatedObject.DragEnter += this.AssociatedObject_DragEnter;
+        this.AssociatedObject.DragLeave += this.AssociatedObject_DragLeave;
+    }
+
+
+    protected override void OnDetaching()
+    {
+        base.OnDetaching();
+        this.AssociatedObject.DragEnter -= this.AssociatedObject_DragEnter;
+        this.AssociatedObject.DragLeave -= this.AssociatedObject_DragLeave;
+    }
+
+    protected override void OnDrop(DragEventArgs e)
+    {
+        //base.OnDrop(e);
+        e.Handled = true;
+        this.SetPreviewOpacity(1.0);
+        this._cacheNode = null;
+
+    }
+
+    private void AssociatedObject_DragLeave(object sender, DragEventArgs e)
+    {
+        this.Cancel();
+    }
+
+    private void AssociatedObject_DragEnter(object sender, DragEventArgs e)
+    {
+        this.Sumit(e);
+        this.SetPreviewOpacity(0.2);
+    }
+
+    private void SetPreviewOpacity(double opacity = 0.2)
+    {
+        if (this._cacheNode == null)
+            return;
+        this._cacheNode.Opacity = opacity;
+        foreach (var item in this._cacheNode.GetParts())
+        {
+            item.Opacity = opacity;
+        }
+    }
+
+    void Cancel()
+    {
+        var diagram = this.AssociatedObject.GetDiagram();
+        if (diagram == null)
+            return;
+        if (this._cacheNode == null)
+            return;
+        diagram.RemoveNode(this._cacheNode);
+        diagram.AddLink(this.AssociatedObject);
+        diagram.AligmentNodes();
+    }
+
+    private Node _cacheNode;
+    protected override void OnDropNodeData(INodeData nodeData, Point offset, Point location)
+    {
+        Node node = this.CreateNodeByData(nodeData);
+        node.Content = nodeData;
+        node.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+        node.Location = new Point(location.X - offset.X + node.DesiredSize.Width / 2, location.Y - offset.Y + node.DesiredSize.Height / 2);
+        var link = this.AssociatedObject;
+        var diagram = this.AssociatedObject.GetDiagram();
+        if (diagram == null)
+            return;
+        diagram.InsertLinkNode(link, node);
+        this._cacheNode = node;
+    }
+
+
+}
---
+++ Source/Controls/H.Controls.Diagram/NodeExtension.cs
@@ -0,0 +1,86 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+namespace H.Controls.Diagram;
+
+public static class NodeExtension
+{
+    public static void Aligment(this IEnumerable<Node> nodes)
+    {
+        Action<IEnumerable<Node>> action = null;
+        action = x =>
+        {
+            foreach (Node node in x)
+            {
+                node.AligmentLayout();
+                List<Node> fromNodes = node.GetToNodes();
+                action.Invoke(fromNodes);
+            }
+        };
+        IEnumerable<Node> ns = nodes.Where(x => x.LinksInto.Count == 0);
+        action.Invoke(ns);
+    }
+
+    public static Part SelectPreivewPart(this Part part)
+    {
+        var find = part.GetPrevious();
+        part.IsSelected = false;
+        find.IsSelected = true;
+        return find;
+    }
+
+    public static Part SelectNextPart(this Part part)
+    {
+        var find = part.GetNext();
+        part.IsSelected = false;
+        find.IsSelected = true;
+        return find;
+    }
+
+    public static Link CreateLinkToNodes(this Node fromNode, Node toNode)
+    {
+        return fromNode == null || toNode == null ? null : fromNode.CreateLinkTo(toNode, null, null);
+    }
+
+    public static Link CreateLinkToPorts(this Node fromNode, Node toNode, Port fromPort, Port toPort)
+    {
+        return fromPort == null || toPort == null ? null : fromNode.CreateLinkTo(toNode, fromPort, toPort);
+    }
+    public static Link CreateLinkTo(this Node fromNode, Node toNode, Port fromPort, Port toPort)
+    {
+        Link link = new Link();
+        link.FromNode = fromNode;
+        link.ToNode = toNode;
+        link.FromPort = fromPort;
+        link.ToPort = toPort;
+        if (fromNode.Content is ILinkInitializer initializer1)
+            initializer1.InitLink(link);
+        if (fromPort?.Content is ILinkInitializer initializer2)
+            initializer2.InitLink(link);
+        if (toNode.Content is ILinkInitializer initializer3)
+            initializer3.InitLink(link);
+        if (toPort?.Content is ILinkInitializer initializer4)
+            initializer4.InitLink(link);
+
+        if (fromNode.Content is ILinkDataCreator creator1)
+            link.Content = creator1.CreateLinkData();
+        if (fromPort?.Content is ILinkDataCreator creator)
+            link.Content = creator.CreateLinkData();
+        if (link.Content is IFlowable)
+        {
+            fromNode.LinksOutOf.Add(link);
+            toNode.LinksInto.Add(link);
+        }
+        else
+        {
+            fromNode.ConnectLinks.Add(link);
+            toNode.ConnectLinks.Add(link);
+        }
+        return link;
+    }
+}
