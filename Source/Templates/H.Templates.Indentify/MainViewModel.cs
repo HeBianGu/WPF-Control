fc416b890a8956466e54c0d6705d80ec0196d6c2
HeBianGu <908293466@qq.com>
2025-06-08 11:40:24 +0800

    ++

---
+++ Source/Templates/H.Templates.Indentify/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using H.Extensions.Mvvm.ViewModels.Base;
+using H.Mvvm.ViewModels.Base;
+
+namespace H.Templates.Indentify;
+internal class MainViewModel : BindableBase
+{
+
+}
