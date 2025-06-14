ffc677288aeebba81f3dce8c43b8a3bd7ea00101
HeBianGu <908293466@qq.com>
2025-06-15 01:01:33 +0800

    ++

--- Source/Extensions/H.Extensions.Command/DeleteCommand.cs
+++ Source/Extensions/H.Extensions.Command/DeleteCommand.cs
@@ -8,6 +8,7 @@
 
 global using H.Services.Message;
 global using System.Collections;
+using System.Reflection.Metadata;
 
 namespace H.Extensions.Command;
 
@@ -15,23 +16,39 @@ namespace H.Extensions.Command;
 [Display(Name = "删除", Description = "从列表中删除当前项目")]
 public class DeleteCommand : DisplayMarkupCommandBase
 {
+    public bool UseDialog { get; set; } = true;
     public override async Task ExecuteAsync(object parameter)
     {
-        await IocMessage.Dialog.ShowDeleteDialog(x =>
-          {
-              if (this.GetTargetElement(parameter) is FrameworkElement element)
-              {
-                  if (element.DataContext == null)
-                      return;
-                  ItemsControl items = element.GetParent<ItemsControl>();
-                  if (items.ItemsSource is IList list)
-                      list.Remove(element.DataContext);
-                  else
-                      items.Items.Remove(element.DataContext);
-              }
-          });
+        if (this.UseDialog)
+        {
+            await IocMessage.Dialog.ShowDeleteDialog(x =>
+            {
+                this.Delete(parameter);
+            });
+        }
+        else
+        {
+            this.Delete(parameter);
+        }
+     
         await base.ExecuteAsync(parameter);
     }
+
+
+    private void Delete(object parameter)
+    {
+        if (this.GetTargetElement(parameter) is FrameworkElement element)
+        {
+            if (element.DataContext == null)
+                return;
+            ItemsControl items = element.GetParent<ItemsControl>();
+            if (items.ItemsSource is IList list)
+                list.Remove(element.DataContext);
+            else
+                items.Items.Remove(element.DataContext);
+        }
+    }
+
 
     public override bool CanExecute(object parameter)
     {
