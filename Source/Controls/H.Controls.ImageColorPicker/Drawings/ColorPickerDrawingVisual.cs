093e97ab99c58e28f5d87fc6eaa7c4a652170cb5
HeBianGu <navylee0210@163.com>
2025-07-07 19:59:00 +0800

    ImageColorPicker+

---
+++ Source/Controls/H.Controls.ImageColorPicker/Drawings/ColorPickerDrawingVisual.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+using System.Diagnostics;
+using System.Windows.Media;
+
+namespace H.Controls.ImageColorPicker.Drawings
+{
+    public class ColorPickerDrawingVisual : DrawingVisual
+    {
+        public Point? Position
+        {
+            get { return (Point?)GetValue(PositionProperty); }
+            set { SetValue(PositionProperty, value); }
+        }
+
+        public static readonly DependencyProperty PositionProperty =
+            DependencyProperty.Register("Position", typeof(Point?), typeof(ColorPickerDrawingVisual), new FrameworkPropertyMetadata(null, (d, e) =>
+            {
+                ColorPickerDrawingVisual control = d as ColorPickerDrawingVisual;
+                if (control == null)
+                    return;
+
+            }));
+
+
+        public Point? PreviewPosition
+        {
+            get { return (Point?)GetValue(PreviewPositionProperty); }
+            set { SetValue(PreviewPositionProperty, value); }
+        }
+
+        public static readonly DependencyProperty PreviewPositionProperty =
+            DependencyProperty.Register("PreviewPosition", typeof(Point?), typeof(ColorPickerDrawingVisual), new FrameworkPropertyMetadata(default(Point?), (d, e) =>
+            {
+                ColorPickerDrawingVisual control = d as ColorPickerDrawingVisual;
+
+                if (control == null)
+                    return;
+            }));
+
+
+        public Color? PreviewColor
+        {
+            get { return (Color?)GetValue(PreviewColorProperty); }
+            set { SetValue(PreviewColorProperty, value); }
+        }
+
+        public static readonly DependencyProperty PreviewColorProperty =
+            DependencyProperty.Register("PreviewColor", typeof(Color?), typeof(ColorPickerDrawingVisual), new FrameworkPropertyMetadata(default(Color?), (d, e) =>
+            {
+                ColorPickerDrawingVisual control = d as ColorPickerDrawingVisual;
+
+                if (control == null) return;
+
+            }));
+
+
+
+        public void Draw()
+        {
+            using var dc = this.RenderOpen();
+
+            if (this.Position.HasValue)
+                dc.DrawHandle(this.Position.Value, Brushes.Chartreuse);
+            if (this.PreviewPosition.HasValue)
+            {
+                dc.DrawHandle(this.PreviewPosition.Value, Brushes.Chartreuse, 1, 2);
+                if (this.PreviewColor != null)
+                    dc.DrawColorText(this.PreviewPosition.Value, this.PreviewColor.Value, this);
+            }
+        }
+    }
+
+    public static class DrawingContextExtension
+    {
+        public static void DrawHandle(this DrawingContext dc, Point point, Brush stroke, double strokeThickness = 1, double len = 6)
+        {
+            double s = len / 2;
+            dc.DrawEllipse(null, new Pen(stroke, strokeThickness), point, len, len);
+        }
+
+        public static void DrawColorText(this DrawingContext dc, Point point, Color color, Visual visual)
+        {
+            FormattedText formattedText = new FormattedText(
+                                    $"{color}",
+                                    System.Globalization.CultureInfo.CurrentCulture,
+                                    FlowDirection.LeftToRight,
+                                    new Typeface("Microsoft YaHei"),
+                                    10,
+                                    Brushes.Chartreuse,
+                                    VisualTreeHelper.GetDpi(visual).PixelsPerDip);
+            dc.DrawText(formattedText, point + new Vector(10, 10));
+        }
+    }
+}
