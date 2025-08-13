5c727bac6956ab003074a8064387d292f95453f1
HeBianGu <navylee0210@163.com>
2025-08-13 17:30:49 +0800

    +

---
+++ Source/Controls/H.Controls.Diagram.Presenters.Workflow/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
+[assembly: XmlnsDefinition("QQ:908293466", "H.Controls.Diagram.Presenters.Workflow")]
+[assembly: XmlnsPrefix("QQ:908293466", "h")]
+
+[assembly: XmlnsDefinition("https://github.com/HeBianGu", "H.Controls.Diagram.Presenters.Workflow.Commands")]
+[assembly: XmlnsDefinition("https://github.com/HeBianGu", "H.Controls.Diagram.Presenters.Workflow.Workflows")]
+[assembly: XmlnsDefinition("https://github.com/HeBianGu", "H.Controls.Diagram.Presenters.Workflow.NodeDatas")]
+[assembly: XmlnsDefinition("https://github.com/HeBianGu", "H.Controls.Diagram.Presenters.Workflow")]
+[assembly: XmlnsPrefix("https://github.com/HeBianGu", "h")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.Diagram.Presenters.Workflow.Commands")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.Diagram.Presenters.Workflow.Workflows")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.Diagram.Presenters.Workflow.NodeDatas")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.Diagram.Presenters.Workflow")]
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "h")]
---
+++ Source/Extensions/H.Extensions.Process/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
+[assembly: XmlnsDefinition("QQ:908293466", "H.Extensions.Process")]
+[assembly: XmlnsPrefix("QQ:908293466", "h")]
+
+[assembly: XmlnsDefinition("https://github.com/HeBianGu", "H.Extensions.Process")]
+[assembly: XmlnsPrefix("https://github.com/HeBianGu", "h")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Extensions.Process")]
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "h")]
