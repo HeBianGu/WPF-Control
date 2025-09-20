89a11ccaa7dbce964f1e204c2e7a27baebe730d2
HeBianGu <navylee0210@163.com>
2025-09-21 00:27:39 +0800

    Presenter+

---
+++ Source/Controls/H.Controls.PagerBox/PageBoxPresenter.xaml.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using H.Extensions.Mvvm.ViewModels.Base;
+using H.Mvvm.Commands;
+using System.ComponentModel;
+
+namespace H.Controls.PagerBox;
+
+public class PageBoxPresenter : DisplayBindableBase
+{
+    private readonly Action _pageUpdated;
+    public PageBoxPresenter(Action pageUpdated)
+    {
+        this._pageUpdated = pageUpdated;
+    }
+    private int _page = 1;
+    [DefaultValue(1)]
+    public int Page
+    {
+        get { return _page; }
+        set
+        {
+            _page = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    private int _limit = 10;
+    [DefaultValue(10)]
+    public int Limit
+    {
+        get { return _limit; }
+        set
+        {
+            _limit = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    private int _totalPage;
+    public int TotalPage
+    {
+        get { return _totalPage; }
+        set
+        {
+            _totalPage = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    private int _total;
+    public int Total
+    {
+        get { return _total; }
+        set
+        {
+            _total = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public RelayCommand SearchCommand => new RelayCommand(x =>
+    {
+        this._pageUpdated?.Invoke();
+    });
+}
---
+++ Source/Presenters/H.Presenters.Common/AddImageListPresenter.xaml.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using H.Extensions.Mvvm.ViewModels.Base;
+using H.Mvvm.Commands;
+using H.Services.Message.IODialog;
+using System.Collections.ObjectModel;
+
+namespace H.Presenters.Common;
+
+public class AddImageListPresenter : DisplayBindableBase
+{
+    private ObservableCollection<string> _paths = new ObservableCollection<string>();
+    public ObservableCollection<string> Paths
+    {
+        get { return _paths; }
+        set
+        {
+            _paths = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public RelayCommand AddFileCommand => new RelayCommand(x =>
+    {
+        var path = IocMessage.IOFileDialog.ShowOpenImageFile();
+        if (path == null)
+            return;
+        this.Paths.Add(path);
+    });
+
+    public RelayCommand RemoveFileCommand => new RelayCommand(x =>
+    {
+        if (x is string t)
+            this.Paths.Remove(t);
+    });
+
+}
