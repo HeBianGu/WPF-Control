ba79f42a6f892908ff611c3dc2cbbae26e334a03
HeBianGu <navylee0210@163.com>
2025-09-26 12:33:51 +0800

    +

---
+++ Source/Styles/H.Style/Controls/Hyperlink.xaml.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+namespace H.Styles.Controls;
+
+//https://learn.microsoft.com/zh-cn/windows/apps/design/style/segoe-ui-symbol-font?WT.mc_id=MVP_380318
+public class HyperlinkKeys
+{
+    public static ComponentResourceKey Default => new ComponentResourceKey(typeof(HyperlinkKeys), "S.Hyperlink.Default");
+
+    //public static ComponentResourceKey SegoeMDL2 => new ComponentResourceKey(typeof(TextBlockIconKeys), "S.TextBlock.Icon.Segoe.MDL2");
+}
