3774297fb67389b886b99e61e89c2ebac4e58edd
HeBianGu <navylee0210@163.com>
2025-09-26 11:16:19 +0800

    ++

--- Source/Styles/H.Style/Controls/TextBox.xaml.cs
+++ Source/Styles/H.Style/Controls/TextBox.xaml.cs
@@ -6,6 +6,7 @@
 // bilibili: https://space.bilibili.com/370266611 
 // Licensed under the MIT License (the "License")
 
+
 namespace H.Styles.Controls;
 
 public class TextBoxKeys
@@ -14,8 +15,30 @@ public class TextBoxKeys
 
     public static ComponentResourceKey Attach => new ComponentResourceKey(typeof(TextBoxKeys), "S.TextBox.Attach");
 
-    //public static ComponentResourceKey Delete => new ComponentResourceKey(typeof(TextBoxKeys), "S.TextBox.Delete");
+    public static ComponentResourceKey Delete => new ComponentResourceKey(typeof(TextBoxKeys), "S.TextBox.Delete");
 
     public static ComponentResourceKey Edit => new ComponentResourceKey(typeof(TextBoxKeys), "S.TextBox.Edit");
 
 }
+
+public class DeleteTextCommandExtension : MarkupExtension, ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+    {
+        return parameter is TextBox text;
+    }
+
+    public void Execute(object parameter)
+    {
+        if (parameter is TextBox text)
+            text.ClearValue(TextBox.TextProperty);
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
+
