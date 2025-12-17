08d7fed6bf85668b883e3a8f878da9e146059ee1
HeBianGu <908293466@qq.com>
2025-12-17 15:06:20 +0800

    +

--- Source/Controls/H.Controls.Form.PropertyItem/Attribute/SourcePropertyItems/BrushesPropertyItemAttribute.cs
+++ Source/Controls/H.Controls.Form.PropertyItem/Attribute/SourcePropertyItems/BrushesPropertyItemAttribute.cs
@@ -45,4 +45,79 @@ namespace H.Controls.Form.PropertyItem.Attribute.SourcePropertyItem
             yield return Brushes.Purple;
         }
     }
+
+    // 荧光色
+    public class FluorescentBrushesPropertyItemAttribute : SourcePropertyItemBaseAttribute
+    {
+
+        public FluorescentBrushesPropertyItemAttribute(Type type) : base(type)
+        {
+
+        }
+
+
+        public override IEnumerable GetSource(PropertyInfo propertyInfo, object obj)
+        {
+            yield return Brushes.Lime;          // #FF00FF00 - 荧光绿
+            yield return Brushes.Cyan;          // #FF00FFFF - 荧光青
+            yield return Brushes.Magenta;       // #FFFF00FF - 荧光紫红
+            yield return Brushes.Yellow;        // #FFFFFF00 - 荧光黄
+            yield return Brushes.Chartreuse;    // #FF7FFF00 - 黄绿色
+        }
+    }
+
+    // 高亮
+    public class HighBrushesPropertyItemAttribute : SourcePropertyItemBaseAttribute
+    {
+
+        public HighBrushesPropertyItemAttribute(Type type) : base(type)
+        {
+
+        }
+
+
+        public override IEnumerable GetSource(PropertyInfo propertyInfo, object obj)
+        {
+            // 透明和中性色
+            yield return Brushes.White;
+            yield return Brushes.Black;
+
+            // 红色系高亮
+            yield return Brushes.Red;
+            yield return Brushes.OrangeRed;
+            yield return Brushes.Tomato;
+            yield return Brushes.Coral;
+            yield return Brushes.HotPink;
+
+            // 橙色/黄色系高亮
+            yield return Brushes.Orange;
+            yield return Brushes.Gold;
+            yield return Brushes.Yellow;
+            yield return Brushes.Goldenrod;
+            yield return Brushes.Khaki;
+
+            // 绿色系高亮
+            yield return Brushes.Lime;
+            yield return Brushes.Chartreuse;
+            yield return Brushes.LawnGreen;
+            yield return Brushes.GreenYellow;
+            yield return Brushes.SpringGreen;
+            yield return Brushes.MediumSpringGreen;
+
+            // 蓝色/青色系高亮
+            yield return Brushes.Cyan;
+            yield return Brushes.Aqua;
+            yield return Brushes.DodgerBlue;
+            yield return Brushes.DeepSkyBlue;
+            yield return Brushes.SkyBlue;
+            yield return Brushes.LightSkyBlue;
+
+            // 紫色/粉色系高亮
+            yield return Brushes.Magenta;
+            yield return Brushes.Fuchsia;
+            yield return Brushes.Orchid;
+            yield return Brushes.Violet;
+            yield return Brushes.MediumPurple;
+        }
+    }
 }
