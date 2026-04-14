04320b2b3fb5e464ffcb7b6e4a38d6e4f635da6a
HeBianGu <908293466@qq.com>
2026-04-14 17:49:27 +0800

    Clip LineGrid+

--- Source/Styles/H.Style/Controls/LineGrid.xaml.cs
+++ Source/Styles/H.Style/Controls/LineGrid.xaml.cs
@@ -6,6 +6,9 @@
 // bilibili: https://space.bilibili.com/370266611 
 // Licensed under the MIT License (the "License")
 
+
+using System.Windows.Controls;
+
 namespace H.Styles.Controls;
 
 public class LineGrid : Grid
@@ -60,6 +63,36 @@ public class LineGrid : Grid
 
         }));
 
+    public CornerRadius LineCornerRadius
+    {
+        get { return (CornerRadius)GetValue(LineCornerRadiusProperty); }
+        set { SetValue(LineCornerRadiusProperty, value); }
+    }
+
+    public static readonly DependencyProperty LineCornerRadiusProperty =
+        DependencyProperty.Register("LineCornerRadius", typeof(CornerRadius), typeof(LineGrid), new FrameworkPropertyMetadata(default(CornerRadius), (d, e) =>
+        {
+            LineGrid control = d as LineGrid;
+
+            if (control == null) return;
+
+            if (e.OldValue is CornerRadius o)
+            {
+
+            }
+
+            if (e.NewValue is CornerRadius n)
+            {
+
+            }
+            control.UpdateClip();
+        }));
+
+    protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+    {
+        base.OnRenderSizeChanged(sizeInfo);
+        this.UpdateClip();
+    }
 
     protected override void OnRender(DrawingContext dc)
     {
@@ -67,16 +100,22 @@ public class LineGrid : Grid
         var pen = new Pen(this.LineStroke ?? SystemColors.ActiveBorderBrush, this.LineThickness);
         if (pen.CanFreeze)
             pen.Freeze();
+        dc.DrawRoundedRectangle(null, pen, new Rect(0, 0, this.ActualWidth, this.ActualHeight), this.LineCornerRadius.TopLeft, this.LineCornerRadius.BottomRight);
         foreach (RowDefinition item in this.RowDefinitions)
-        {
             dc.DrawLine(pen, new Point(0, item.Offset), new Point(this.ActualWidth, item.Offset));
-        }
-        dc.DrawLine(pen, new Point(0, this.ActualHeight), new Point(this.ActualWidth, this.ActualHeight));
-
         foreach (ColumnDefinition item in this.ColumnDefinitions)
-        {
             dc.DrawLine(pen, new Point(item.Offset, 0), new Point(item.Offset, this.ActualHeight));
+    }
+
+    private void UpdateClip()
+    {
+        if (this.LineCornerRadius == default(CornerRadius))
+        {
+            this.Clip = null;
+            return;
         }
-        dc.DrawLine(pen, new Point(this.ActualWidth, 0), new Point(this.ActualWidth, this.ActualHeight));
+        Rect rect = new Rect(this.RenderSize);
+        double radius = this.LineCornerRadius.TopLeft; // Assuming uniform corner radius
+        this.Clip = new RectangleGeometry(rect, radius, radius);
     }
 }
