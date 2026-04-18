e4f13cd43a30bcafd540749d06a74270c0378690
HeBianGu <908293466@qq.com>
2026-04-18 19:21:52 +0800

    +

--- Source/Controls/H.Controls.TagBox/TagBox.xaml.cs
+++ Source/Controls/H.Controls.TagBox/TagBox.xaml.cs
@@ -157,7 +157,7 @@ namespace H.Controls.TagBox
             var values = this.Tags.Split(TagOptions.Instance.SplitChars.ToCharArray());
             foreach (var value in values)
             {
-                var find = this.ItemsSource.OfType<ITag>().FirstOrDefault(x => x.Name == value);
+                var find = this.ItemsSource?.OfType<ITag>().FirstOrDefault(x => x.Name == value);
                 if (find == null)
                     continue;
                 this.SelectedItems.Add(find);
