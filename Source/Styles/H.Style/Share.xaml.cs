b65a40e3539ed91e666c9c589c910e8a79f2536c
HeBianGu <908293466@qq.com>
2026-04-22 11:24:46 +0800

    bug+

---
+++ Source/Styles/H.Style/Share.xaml.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H.Styles;
+public class ShareKeys
+{
+    public static ComponentResourceKey BorderTemplate => new ComponentResourceKey(typeof(BorderKeys), "S.Border.TemplatedParent");
+}
