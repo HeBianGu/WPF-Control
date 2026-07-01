6183fed9a4d2bd715f78b7d0797ffc2f6e3350a7
HeBianGu <908293466@qq.com>
2026-07-01 16:07:59 +0800

    ComboBox重构+

--- Source/Styles/H.Style/Controls/--ComboBox.xaml.cs
+++ Source/Styles/H.Style/Controls/--ComboBox.xaml.cs
@@ -8,12 +8,12 @@
 
 namespace H.Styles.Controls;
 
-//public class ComboBoxKeys
-//{
-//    public static ComponentResourceKey Default => new ComponentResourceKey(typeof(ComboBoxKeys), "S.ComboBox.Default");
-//}
+public class ComboBoxKeys
+{
+    public static ComponentResourceKey Default => new ComponentResourceKey(typeof(ComboBoxKeys), "S.ComboBox.Default");
+}
 
-//public class ComboBoxItemKeys
-//{
-//    public static ComponentResourceKey Default => new ComponentResourceKey(typeof(ComboBoxItemKeys), "S.ComboBoxItem.Default");
-//}
+public class ComboBoxItemKeys
+{
+    public static ComponentResourceKey Default => new ComponentResourceKey(typeof(ComboBoxItemKeys), "S.ComboBoxItem.Default");
+}
