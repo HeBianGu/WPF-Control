f1002db3f71aead7e2ce48cc5624aed0da23acf8
HeBianGu <908293466@qq.com>
2026-08-10 19:24:54 +0800

    +

--- Source/Controls/H.Controls.ShapeBox/AssemblyInfo.cs
+++ Source/Controls/H.Controls.ShapeBox/AssemblyInfo.cs
@@ -23,6 +23,7 @@ using System.Windows.Markup;
 [assembly: XmlnsDefinition("https://github.com/HeBianGu", "H.Controls.ShapeBox")]
 [assembly: XmlnsPrefix("https://github.com/HeBianGu", "h")]
 
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.ShapeBox.State")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.ShapeBox.Settables.ViewSettables")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.ShapeBox.Settables")]
 [assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "H.Controls.ShapeBox.Shapes")]
