3f5b41d6c0ab1fba711b456ab3d080ac484ff94a
HeBianGu <navylee0210@163.com>
2026-09-23 17:11:46 +0800

    +

---
+++ Source/Tests/H.Test.Plugin/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+using H.Extensions.ApplicationBase;
+using H.Modules.Plugin;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace H.Test.Plugin;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : H.Extensions.ApplicationBase.ApplicationBase
+{
+    protected override void ConfigureServices(IServiceCollection services)
+    {
+        services.AddPlugin();
+    }
+
+    protected override System.Windows.Window CreateMainWindow(StartupEventArgs e)
+    {
+        return new MainWindow();
+    }
+
+}
---
+++ Source/Tests/H.Test.Plugin/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) HeBianGu Authors. All Rights Reserved. 
+// Author: HeBianGu 
+// Github: https://github.com/HeBianGu/WPF-Control 
+// Document: https://hebiangu.github.io/WPF-Control-Docs  
+// QQ:908293466 Group:971261058 
+// bilibili: https://space.bilibili.com/370266611 
+// Licensed under the MIT License (the "License")
+
+global using H.Iocable;
+using H.Modules.Plugin;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace H.Test.Plugin;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : System.Windows.Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var groups = PluginManager.GetPluginInstances<IPlugin>();
+        var count = groups.Count();
+        MessageBox.Show("查找到" + count);
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+
+    }
+}
+
+public interface IPlugin
+{
+}
+
+public class Plugin : IPlugin
+{
+}
